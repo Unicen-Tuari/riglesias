c875a757c25fa926dc74184ce3f568fb20472e88
VirutaMaster <virubiru@hotmail.com>
2016-04-22 15:47:37 -0300

    actualizacion: se agrego img de croupier

--- ruleta/js/engine.js
+++ ruleta/js/engine.js
@@ -11,6 +11,17 @@ var penalty=0;
 function pay_cash(){
   cash+=10;
 }
+/*-------------------------------------------------------*/
+function debit_cash(){
+  if(penalty==3){
+    penalty=0;
+    cash-=2;
+    printMsg('Perdió 3 veces seguidas, se le cobrará doble penalización!');
+  }else{
+    cash-=1;
+    printMsg('Usted perdió! se le penaliza con -1 credito');
+  }
+}
 
 /*-------------------------------------------------------*/
 function selectNum(num){
@@ -41,6 +52,8 @@ function play(){
   //bet=document.getElementById('bet').value;
   if(selected==-1){
     printMsg('Debe seleccionar un numero del tablero');
+  }else if(total_bet==0){
+    printMsg('Debe Realizar una apuesta antes de Girar la Ruleta!');
   }else{
     num=Math.floor((Math.random() * 10));
     printMsg('Salió el nro: '+num+'');
@@ -49,18 +62,13 @@ function play(){
       printMsg('Usted ganó!');
     }else{
       penalty++;
-      if(penalty==3){
-        penalty=0;
-        cash-=1;
-        printMsg('Se le cobrará 1 crédito por penalización!');
-      }
-      printMsg('Usted perdió!');
+      debit_cash();
+
     }
     printMsg('Haga su apuesta!');
     document.getElementById('saldo').innerHTML=cash;
     selected=-1;
     total_bet=0;
-    console.log(cash);
   }
 
 }
