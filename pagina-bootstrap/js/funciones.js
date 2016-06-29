506e06fdafc6f984b5bdf7fb55b6ddbd2dc9bab9
VirutaMaster <virubiru@hotmail.com>
2016-06-29 16:02:13 -0300

    Se termino la ruleta, y se aplico ajax en la web

--- pagina-bootstrap/js/funciones.js
+++ pagina-bootstrap/js/funciones.js
@@ -1,6 +1,87 @@
 $(window).on('scroll',function(){
-  if($(document).scrollTop()>160){
+  if($(document).scrollTop()>$('#carousel-example-generic').height()){
     $('#nav-princ').addClass('navbar-fixed-top');
   }
   else $('#nav-princ').removeClass('navbar-fixed-top');
+});
+
+function cargaDatos(seccion){
+  $('#contenedor-data').html("cargando...");
+  $.ajax({
+    url:'sections/'+seccion+'.html',
+    dataType: 'HTML',
+    method: 'GET',
+    error: function(){
+      alert("la carga fallo");
+    },
+    success: function(data){
+      $('#contenedor-data').html(data);
+      $('[data-toggle="tooltip"]').tooltip();//inicializo los tooltips que contenga el html que levante el ajax
+    }
+
+  });
+}
+
+
+function muestraDatos(datos,ids){
+  var tbody = '<table class="tabla-comp table"><thead><tr><th>Caracteristicas</th><th>Producto 1</th><th>Producto 2</th></tr></thead><tbody>';
+  tbody+='<tr><th>Actividad</th><td>'+(datos.information[ids[0]].thing.actividad)+'</td><td>'+(datos.information[ids[1]].thing.actividad)+'</td></tr>';
+  tbody+='<tr><th>Costuras</th><td>'+(datos.information[ids[0]].thing.costuras)+'</td><td>'+(datos.information[ids[1]].thing.costuras)+'</td></tr>';
+  tbody+='<tr><th>UPF</th><td>'+(datos.information[ids[0]].thing.upf)+'</td><td>'+(datos.information[ids[1]].thing.upf)+'</td></tr>';
+  tbody+='<tr><th>Varillas</th><td>'+(datos.information[ids[0]].thing.varillas)+'</td><td>'+(datos.information[ids[1]].thing.varillas)+'</td></tr>';
+  tbody+='<tr><th>Personas</th><td>'+(datos.information[ids[0]].thing.personas)+'</td><td>'+(datos.information[ids[1]].thing.personas)+'</td></tr>';
+  tbody+='<tr><th>Filtro</th><td>'+(datos.information[ids[0]].thing.filtro)+'</td><td>'+(datos.information[ids[1]].thing.filtro)+'</td></tr>';
+  tbody+='<tr><th>Peso</th><td>'+(datos.information[ids[0]].thing.peso)+'</td><td>'+(datos.information[ids[1]].thing.peso)+'</td></tr>';
+  tbody+='<tr><th>Impermeabilidad</th><td>'+(datos.information[ids[0]].thing.imperm)+'</td><td>'+(datos.information[ids[1]].thing.imperm)+'</td></tr>';
+  tbody+='</tbody></table>';
+  $('#contenedor-data').html(tbody);
+}
+
+function getInformationByGroup(groupid,ids){
+  $.ajax({
+    url:"http://web-unicen.herokuapp.com/api/group/" + groupid,
+    method:"GET",
+    contentType: "application/json; charset=utf-8",
+    success: function(resultData){
+      muestraDatos(resultData,ids);
+    },
+    error:function(jqxml, status, errorThrown){
+      alert('Error!');
+      console.log(errorThrown);
+    }
+  });
+}
+
+$(document).ready(function(){
+  $('#home').on('click',function(){
+    cargaDatos('news');
+  });
+
+  $('#productos').on('click',function(){
+    cargaDatos('productos');
+    $('.navbar-nav li').removeClass('active');
+    $('#productos').parent().addClass('active');
+    console.log($(this).attr('id'));
+  });
+
+  $('body').on('click','#comparar',function(){
+    $('#ok').toggle();
+    $('.carpas .compare-check').toggle();
+  });
+
+  $('body').on('click','#ok',function(){
+    if($('.carpas .compare-check:checked').length==2){
+      var x='';
+      var ids = [];
+      $('.carpas .compare-check:checked').each(function(){
+        x=parseInt($(this).attr('idprod'));
+        ids.push(x);
+      });
+
+      $('#contenedor-data').html('<div class="row"><img src="img/loading.gif"></div>');
+      getInformationByGroup(77,ids);
+    }else console.log("Solo puede seleccionar 2 productos");
+  });
+
+
 });
--- ruleta-2/js/engine.js
+++ ruleta-2/js/engine.js
@@ -1,9 +1,17 @@
 "use strict"
-
+var val_pleno=36;
+var val_cero=42;
+var val_chance=2;
+var chip_selected=0;
+var jugador=null;
+var game=null;
+var rango=5;
+/////////////////////////////////
 $(document).ready(function(){
   console.log("cargado");
 
   $('#btn-newgame').click(function(){
+    inicializar();
     game.Start();
     $('#overlay-cortina').hide();
   });
@@ -16,62 +24,164 @@ $(document).ready(function(){
     var nro = parseInt(($(this).index('.numbtn')));
     game.Apostar(nro);
   });
+
+  $('#chips-panel div').click(function(){
+
+    var pepe=($(this).attr('id'));
+    //$('#'+pepe).hide();
+    chip_selected=parseInt($(this).attr('valor'));
+    console.log(chip_selected);
+  });
+
 });
+
+
 ////////////////// obj game ///////////////////////
 function Game(){
   this.inic = false;
   this.hasbet = false;
   this.error = '';
   this.cameout = -1;
-  this.arr_nums = [];
-
+  this.bets = [];
+  this.bets_rev = [];
+  this.revancha = true;
+  this.colour = '';
+  this.range = '';
+  this.odds = '';
+  this.reds = [1,3,5,6,8];
 }
 
 Game.prototype.Start = function(){
-  console.log("el juego ha comenzado!");
+    $('#msg-panel').html('<p>el juego ha comenzado!</p>');
   this.inic = true;
-  this.arr_nums = [0,0,0,0,0,0,0,0,0,0,0];
+  this.bets = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
   $('#saldo').html(jugador.saldo());
+  $("#rango").on('change',function(){console.log("peop")});
 };
 
 Game.prototype.Girar = function(){
       var num=(Math.floor((Math.random() * 11)));
-      console.log("salio el: "+num);//borrar
+      $('#msg-panel').prepend('<p>salió el número: '+num+'</p>');//borrar
       this.cameout=num;
+      $(".numbtn").eq(parseInt(this.cameout)).addClass('winner');
+      console.log($(".numbtn").eq(parseInt(this.cameout)));
 };
 
 Game.prototype.Apostar = function(nro){
-  this.hasbet=true;
-  this.arr_nums[nro]++;
-  console.log(this.arr_nums);//borrar
-
+  if((jugador.saldo()-chip_selected)>=0){
+    this.hasbet=true;
+    this.bets[nro]+=chip_selected;
+    jugador.debit(chip_selected);
+      console.log(jugador.saldo());//borrar
+      console.log(this.bets);//borrar
+    $('#saldo').html(jugador.saldo());
+  }else $('#msg-panel').prepend('<p>Saldo insuficiente!</p>');
 };
 
-Game.prototype.Refresh = function(){
-  this.arr_nums = [0,0,0,0,0,0,0,0,0,0,0];
-  this.cameout = -1;
-  this.hasbet = false;
+Game.prototype.isRed = function(){
+  for(var i=0;i<(this.reds).length;i++){
+    if(this.reds[i]==this.cameout){
+      return true;
+    }
+   }
+};
+
+Game.prototype.isOdd = function(){
+  if((this.cameout % 2)==0){
+    return true;
+  }else return false;
 }
 
-Game.prototype.Transaction = function(){
-  if(this.arr_nums[this.cameout]!=0){
-    jugador.credit(333);
-    console.log('ganaste!! :) ');
-  }else{
-    jugador.debit(33);
-    console.log('perdiste :( !!');
-  }
-  $('#saldo').html(jugador.saldo());
+Game.prototype.Refresh = function(){
+  //$(".numbtn").eq(this.cameout).removeClass('winner');
+  this.bets_rev=this.bets;
+  this.bets = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+  this.cameout = -1;
+  this.hasbet = false;
+  chip_selected=0;
+  console.log('ronda finalizada');
+  console.log(this.bets_rev);
+  setTimeout(function(){ 
+    $(".numbtn").eq(this.cameout).removeClass('winner');
+  }, 3000);
 }
+
+Game.prototype.Resultado = function(){
+  var sum=0;
+
+  if((this.cameout)!=0){
+    //color
+    if((this.bets[11]!=0)&& this.isRed()){
+      sum+=(this.bets[11])*val_chance;
+    }else if((this.bets[12]!=0)&& !this.isRed()){
+      sum+=(this.bets[12])*val_chance;
+    }
+
+    //paridad
+    if((this.bets[13]!=0)&& this.isOdd()){
+      sum+=(this.bets[13])*val_chance;
+    }else if((this.bets[14]!=0)&& !this.isOdd()){
+      sum+=(this.bets[14])*val_chance;
+    }
+
+    //rango
+    if((this.bets[15]!=0)&&(this.cameout<rango)){
+      sum+=(this.bets[15])*val_chance;
+    }else if((this.bets[16]!=0)&&(this.cameout>rango)){
+      sum+=(this.bets[16])*val_chance;
+    }
+  }
+
+  //plenos
+  if(this.cameout==0){
+    sum+=(this.bets[0]*val_cero);
+  }else sum+=(this.bets[this.cameout]*val_pleno);
+
+
+  return sum;
+};
+
+Game.prototype.Revancha = function(){
+  this.bets=this.bets_rev;
+  console.log(this.bets);
+  this.hasbet=true;
+  this.revancha=false;
+  this.Jugada();
+  this.revancha=true;
+};
+
+Game.prototype.Transaction = function(){
+  var monto=this.Resultado();
+    jugador.credit(monto);
+
+    if(monto==0){
+      $('#msg-panel').prepend('<p>Usted perdió</p>');
+      if(this.revancha==true){
+        $('#btn-revancha').show();
+        $('#msg-panel').prepend('<p>Usted dispone de un tiro de Revancha!</p>');
+        $('#btn-revancha').on('click',function(){
+          game.Revancha();
+          $(this).hide();
+        });
+      }
+    }else $('#msg-panel').prepend('<p>Usted ganó: '+monto+' creditos</p>');
+
+  $('#saldo').html(jugador.saldo());
+};
 
 Game.prototype.Jugada = function(){
   if(this.inic){
     if(this.hasbet){
       this.Girar();
+
       this.Transaction();
-      this.Refresh();
-    }else console.log('Debe Realizar una apuesta');
-  }else console.log('Debe iniciar un Nuevo Juego');
+
+      if(jugador.saldo()==0){
+        $('#msg-panel').prepend('<p>su juego ha terminado, lo siento!</p>');
+        finalizar();
+      }else this.Refresh();
+    }else $('#msg-panel').prepend('<p>Debe Realizar una apuesta</p>');
+  }else $('#msg-panel').prepend('<p>Debe iniciar un Nuevo Juego</p>');
 };
 
 ///////////////////////// obj jugador ///////////////////////////////
@@ -95,5 +205,19 @@ function Jugador(nombre,apellido){
 
 
 ///////////////////// init ///////////////////////////
-var jugador = new Jugador('pepe','rodriguez');
-var game = new Game();
+
+function inicializar(){
+  jugador = new Jugador('pepe','rodriguez');
+  game = new Game();
+  var juke = document.getElementById("jukebox");
+  juke.volume = 0.2;
+  juke.play();
+}
+
+function finalizar(){
+  game=null;
+  jugador=null;
+  $('#overlay-cortina').show();
+  var juke = document.getElementById("jukebox");
+  juke.pause();
+}
