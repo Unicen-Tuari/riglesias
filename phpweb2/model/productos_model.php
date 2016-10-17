1e0260fa35f11c7abe27caa3a7f999d73e884e9d
VirutaMaster <virubiru@hotmail.com>
2016-10-16 22:57:57 -0300

    se agregaron funcionalidades del admin closes #13

--- phpweb2/model/productos_model.php
+++ phpweb2/model/productos_model.php
@@ -27,6 +27,27 @@ class productos_model
     //print_r($item);
     return $item;
   }
+
+  public function copyImage($image){
+    $path = '../img/prods/'.uniqid().$image["name"];
+    move_uploaded_file($image["tmp_name"], $path);
+    return $path;
+  }
+
+  public function add_prod($id,$nombre,$cat,$precio,$stock,$descr,$arr_img){
+    // echo "id=".$id."nombre=".$nombre."cat=".$cat."precio=".$precio."stock=".$stock."descripcion=".$descr;
+    $insert = $this->db->prepare("INSERT INTO productos(id,nombre,precio,descripcion,categoria,stock) VALUES(?,?,?,?,?,?)");
+    $insert->execute(array($id,$nombre,$precio,$descr,$cat,$stock));
+
+    foreach ($arr_img as $image){
+      $path_image =  $this->copyImage($image);
+      $insert = $this->db->prepare("INSERT INTO prod_images(id, prod_id, imgsrc) VALUES(?,?,?)");
+      $insert->execute(array(null,$id,$path_image));
+    }
+
+
+
+  }
 }
 
 
--- phpweb2/view/productos_view.php
+++ phpweb2/view/productos_view.php
@@ -13,15 +13,37 @@ class productos_view
     $this->smarty=new Smarty;
   }
 
+  public function show_home_admin(){
+    $this->smarty->display('../templates/admin.tpl');
+  }
+  public function show_form(){
+    $this->smarty->display('../templates/add_product.tpl');
+  }
+
+  public function show_home_admin_body(){
+    $this->smarty->display('../templates/admin_body_home.tpl');
+  }
+
+  public function show_table_products($productos){
+    //print_r($productos);
+    $this->smarty->assign('productos',$productos);
+    //$this->smarty->display('../templates/table_productos.tpl');
+    $this->smarty->display('../templates/table_productos.tpl');
+  }
+
   public function show_products($productos){
+    //print_r($productos);
     $this->smarty->assign('productos',$productos);
     $this->smarty->display('productos.tpl');
   }
 
   public function show_item($item){
-    $this->smarty->assign('item',$item);
+    //print_r($item);
+    $this->smarty->assign('producto',$item);
     $this->smarty->display('item.tpl');
   }
+
+
 }
 
 
